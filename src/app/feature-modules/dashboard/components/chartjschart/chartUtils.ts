5e10a36632f75645b073118b52d1fa5d72bc6e33
andrejristevski <andrej.rist@gmail.com>
2018-04-26 11:58:00 +0200

    Improvements in exchanges

--- src/app/feature-modules/dashboard/components/chartjschart/chartUtils.ts
+++ src/app/feature-modules/dashboard/components/chartjschart/chartUtils.ts
@@ -11,14 +11,15 @@ export class ChartUtils {
        return ds.map(set => {
             return {
                 label: `${set.inpCur} to ${set.outputCur}`,
-                strokeColor: "black",
+                strokeColor: 'black',
                 fill: false,
                 data: set.rates,
-                borderColor: `rgb(${Math.floor(Math.random() * 2) + 55}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 200) + 55})`,
+                borderColor: `rgb(${Math.floor(Math.random() * 2) + 55},
+                 ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 200) + 55})`,
                 lineTension: 0.1,
                 radius: 1
             };
 
-        })
+        });
     }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,14 +7,6 @@ export const environment = {
   percentagesum: 'percentagesum',
   initialInputCur: 'EUR',
   initialOutputCur: 'AUD',
-  firebase: {
-    apiKey: 'AIzaSyDu_gbNfdaLrF0RTJayYUlhs78nW1jT3O0',
-    authDomain: 'andrej-finance.firebaseapp.com',
-    databaseURL: 'https://andrej-finance.firebaseio.com',
-    projectId: 'andrej-finance',
-    storageBucket: 'andrej-finance.appspot.com',
-    messagingSenderId: '637776522432'
-  }
 };
 export const ChartType = {
   'PAIR': 'PairData',
