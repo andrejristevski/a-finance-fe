253643a331890539a96d2e71b599a69b7ee14efc
andrejristevski <andrej.rist@gmail.com>
2018-04-26 12:19:02 +0200

    Refactor chart js component to helpers

--- src/app/feature-modules/dashboard/dashboard.module.ts
+++ src/app/feature-modules/dashboard/dashboard.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CreateChartComponent } from './components/create-chart/create-chart.component';
-import { ChartComponent } from './components/chartjschart/chartjschart.component';
 import { TimeframeButtonsComponent } from './components/timeframe-buttons/timeframe-buttons.component';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
@@ -21,7 +20,7 @@ import { HelpersModule } from '../helpers/helpers.module';
     HelpersModule,
     DashboardRoutingModule
   ],
-  declarations: [DashboardComponent, CreateChartComponent, ChartComponent,
+  declarations: [DashboardComponent, CreateChartComponent, 
     TimeframeButtonsComponent]
 })
 export class DashboardModule { }
--- src/app/feature-modules/helpers/helpers.module.ts
+++ src/app/feature-modules/helpers/helpers.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ToggleViewComponent } from './components/toggle-view/toggle-view.component';
+import { ChartComponent } from './components/chartjschart/chartjschart.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ToggleViewComponent],
-  exports: [ToggleViewComponent]
+  declarations: [ToggleViewComponent, ChartComponent],
+  exports: [ToggleViewComponent, ChartComponent]
 })
 export class HelpersModule { }
